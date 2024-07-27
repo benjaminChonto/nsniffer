06137bceb3c33dc5f9500ece7cdcdd6f27bed4f7
Benjamin Csonto <csonto.benjamin@gmail.com>
2024-07-27 02:06:51 +0200

    capture in a separate thread

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     implementation("org.pcap4j:pcap4j-core:1.8.2")
     implementation("org.pcap4j:pcap4j-packetfactory-static:1.8.2")
     implementation("ch.qos.logback:logback-classic:1.5.6")
+    implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
     testImplementation(kotlin("test"))
 }
 
